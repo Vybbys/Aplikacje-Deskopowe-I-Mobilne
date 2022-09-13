842e53f1022d4a7649e3990d9cee290299c02279
essa <essa@gmail.com>
2022-09-13 14:01:55 +0200

    Dodalem plik z testowym programem

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;     
+                   
+namespace Test_Visual_Studio {
+    class Program {
+                   
+        static void Main(string[] args) {
+                                    
+            Console.WriteLine("Hello World!");
+        }         
+    };             
+}                  
