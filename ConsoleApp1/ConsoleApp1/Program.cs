43116814d73db7647067516e0cf1fba6e42040f3
essa <essa@gmail.com>
2022-09-15 12:23:07 +0500

    Pare Testowych Linijek

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            string imie = "Jan";
+            string nazwisko = "Kowalski";
+
+            Console.WriteLine("Witaj " + imie + " " + nazwisko + " " + "tutaj");
+            Console.WriteLine("Witaj {0} {0} {0} tutaj", imie, nazwisko);
+            Console.WriteLine($"Witaj {imie} {nazwisko} tutaj");
+            //imie.length();
+
+            Console.WriteLine("Predkosc to km\\h");
+            Console.WriteLine(@"Predkosc to km\h");
+
+            int liczba = 5;
+            double liczba1 = 132.12;
+
+
+        }
+    }
+}
