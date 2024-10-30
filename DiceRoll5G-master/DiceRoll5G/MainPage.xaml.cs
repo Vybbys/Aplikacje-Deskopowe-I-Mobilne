3e327f6423425c741ce766e27cc8893baecd16b7
Vybbys <105807152+Vybbys@users.noreply.github.com>
2024-10-30 09:01:49 +0100

    Add files via upload

---
+++ DiceRoll5G-master/DiceRoll5G/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿namespace DiceRoll5G
+{
+    public partial class MainPage : ContentPage
+    {
+        int gameResult = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DiceRoll(object sender, EventArgs e)
+        {
+            int[] dice = new int[5];
+            Random r = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                dice[i] = r.Next(1, 7);
+            }
+            Dice0.Source = "k6_" + dice[0] + ".png";
+            Dice1.Source = "k6_" + dice[1] + ".png";
+            Dice2.Source = "k6_" + dice[2] + ".png";
+            Dice3.Source = "k6_" + dice[3] + ".png";
+            Dice4.Source = "k6_" + dice[4] + ".png";
+            int rollResult = 0;
+            for(int dots = 1; dots <= 6; dots++)
+            {
+                int count = 0;
+                for(int diceIndex = 0; diceIndex < 5; diceIndex++)
+                {
+                    if (dice[diceIndex] == dots)
+                    {
+                        count++;
+                    }
+                }
+                if(count > 1)
+                {
+                    rollResult += dots * count;
+                }
+            }
+            RollResultLabel.Text = "Wynik tego losowania: " + rollResult;
+            gameResult += rollResult;
+            GameResultLabel.Text = "Wynik gry: " + gameResult;
+        }
+        /* Funkcja ResetGame
+         * Funkcja resetuje interfejs gry i punktacje do stanu początkowego
+         * Parametry: sender - obiekt wywołujący zdarzenie
+         *            e - argumenty zdarzenia
+         * Autor: 00000000000
+         */
+        private void ResetGame(object sender, EventArgs e)
+        {
+            Dice0.Source = "k6_0.png";
+            Dice1.Source = "k6_0.png";
+            Dice2.Source = "k6_0.png";
+            Dice3.Source = "k6_0.png";
+            Dice4.Source = "k6_0.png";
+
+            gameResult = 0;
+            RollResultLabel.Text = "Wynik tego losowania: 0";
+            GameResultLabel.Text = "Wynik gry: " + gameResult;
+        }
+    }
+
+}
